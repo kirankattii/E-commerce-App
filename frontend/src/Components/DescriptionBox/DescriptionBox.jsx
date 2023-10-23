f91a02d2597ba0991dbf6f92df9f97f7f6220348
kirankattii <kirankatti550@gmail.com>
2023-10-23 21:59:06 +0530

    Ecommerce APP

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="description-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt
+          fugit laborum odit, velit nisi earum quae voluptate ad veniam minima,
+          aperiam ullam temporibus, eaque consequuntur quas illum odio labore
+          ratione harum? Accusamus, consectetur reprehenderit illum excepturi
+          molestias voluptatibus? Dicta dolor provident facere laborum
+          praesentium blanditiis animi, porro aperiam vel rem?
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis
+          soluta eligendi ipsa laboriosam obcaecati minima explicabo atque ut
+          perspiciatis dicta. Dolorem, provident nam tempora iste, facilis sit
+          dolor saepe et, libero doloremque placeat officiis? Soluta repellat
+          ratione ut maiores placeat rerum libero harum animi ipsa aut. Quod
+          dolorum eius velit.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
